ca577414df7fd9d5260285d107dd267b88d19310
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2024-09-17 00:54:21 +0200

    Include kernel files upon setup

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ import setuptools
 
 if __name__ == "__main__":
     # Still necessary, otherwise we get a pip error
-    setuptools.setup()
+    setuptools.setup(include_package_data=True,
+                     # Important to also install .cpp/.h/.cu files that are in torch_utils/ops
+                     package_data={'': ['*.cpp', '*.cu', '*.h']})
